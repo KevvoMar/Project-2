206770bcae55b1863a390dc160b1b0e6100bf223
KevvoMar <marrerokev25@GMAIL.COM>
2026-03-23 15:23:47 -0400

    modals

---
+++ app.js
@@ -0,0 +1,13 @@
+function contact(event) {
+    event.preventDefault();
+
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible";
+        console.log('it worked 1')
+    }, 1000);
+
+}
